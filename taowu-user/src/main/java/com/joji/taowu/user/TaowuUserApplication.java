5d066b0bd14d743437f2033e28a148b78a7a5ad7
YangZhiXiong <641484973@qq.com>
2023-03-27 11:39:32 +0800

    淘物商城用户模块

---
+++ taowu-user/src/main/java/com/joji/taowu/user/TaowuUserApplication.java
@@ -0,0 +1,13 @@
+package com.joji.taowu.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaowuUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaowuUserApplication.class, args);
+	}
+
+}
---
+++ taowu-user/src/test/java/com/joji/taowu/user/TaowuUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joji.taowu.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaowuUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
