8b8a3b60fa8113317f9be89bb307b384d376edc0
YangZhiXiong <641484973@qq.com>
2023-04-27 15:15:22 +0800

    订单服务跟新

--- taowu-admin/src/main/java/com/joji/taowu/admin/controller/OrderController.java
+++ taowu-admin/src/main/java/com/joji/taowu/admin/controller/OrderController.java
@@ -2,12 +2,8 @@ package com.joji.taowu.admin.controller;
 
 
 import com.joji.taowu.admin.service.OrderService;
-import com.joji.taowu.common.param.PageParam;
 import com.joji.taowu.common.param.ProductSearchParam;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 /**
@@ -21,8 +17,8 @@ public class OrderController {
     @Resource
     private OrderService orderService;
 
-    @GetMapping("list")
-    public Object list(ProductSearchParam productSearchParam){
+    @PostMapping("list")
+    public Object list(@RequestBody ProductSearchParam productSearchParam){
 
         return orderService.list(productSearchParam);
     }
